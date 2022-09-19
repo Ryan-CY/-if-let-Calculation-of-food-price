e08454ececa3a3b014bc9970830a4c60e314fbeb
Ryan CY <phillin820109@msn.com>
2022-09-19 11:01:48 +0800

    Initial Commit

---
+++ food price in Singapore at cafes n restaurants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  food price in Singapore at cafes n restaurants
+//
+//  Created by Ryan Lin on 2022/9/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
